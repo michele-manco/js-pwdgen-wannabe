2a0828f14f5e259ac0a3a76a436b5d5268cb27f2
Michele_Manco <motlamik@gmail.com>
2019-11-13 20:43:08 +0100

    firs steps

---
+++ main.js
@@ -0,0 +1,11 @@
+var nome=prompt('quale è il tuo nome');
+console.log(nome);
+document.getElementById('nome').innerHTML = nome;
+var cognome=prompt('quale è il tuo cognome');
+console.log(cognome);
+document.getElementById('cognome').innerHTML = cognome;
+var colore_preferito=prompt('quale è il tuo colore preferito');
+console.log(colore_preferito)
+document.getElementById('colore_preferito').innerHTML = colore_preferito;
+var numero=19;
+console.log(numero);
